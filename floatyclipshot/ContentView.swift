27deb4feea0d157d6ba36d01b5d68e1aa306caa7
hooshyar <hooseyr@gmail.com>
2025-11-21 15:36:51 +0300

    Initial Commit

---
+++ floatyclipshot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  floatyclipshot
+//
+//  Created by Hooshyar on 11/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ floatyclipshot/floatyclipshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  floatyclipshotApp.swift
+//  floatyclipshot
+//
+//  Created by Hooshyar on 11/21/25.
+//
+
+import SwiftUI
+
+@main
+struct floatyclipshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
